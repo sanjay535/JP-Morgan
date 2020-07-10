7039db9eb2640a4015413350b196edd27c24bff8
sanjay gautam <34466733+sanjay535@users.noreply.github.com>
2020-07-10 18:24:09 +0600

    task3
    completed task3

---
+++ DataManipulator.ts
@@ -0,0 +1,32 @@
+import { ServerRespond } from './DataStreamer';
+
+export interface Row {
+  price_abc:number,
+  price_def:number,
+  ratio:number,
+  timestamp: Date,
+  upper_bound:number,
+  lower_bound:number,
+  trigger_alert:number | undefined,
+}
+
+
+export class DataManipulator {
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const priceABC=(serverRespond[0].top_ask.price + serverRespond[0].top_bid.price)/2;
+    const priceDEF=(serverRespond[1].top_ask.price + serverRespond[1].top_bid.price)/2;
+    const ratio=priceABC/priceDEF;
+    const upper_Bound=1+0.05;
+    const lower_Bound=1-0.05;
+ return{
+  price_abc:priceABC,
+  price_def:priceDEF,
+  ratio,
+  timestamp:serverRespond[0].timestamp > serverRespond[1].timestamp ?serverRespond[0].timestamp:serverRespond[1].timestamp,
+  upper_bound:upper_Bound,
+  lower_bound:lower_Bound,
+  trigger_alert:(ratio>upper_Bound || ratio<lower_Bound)? ratio:undefined,
+  };
+ }
+}
+
